8d229fa384ed8065996a488410c2b55236c1407d
Jonathan Pyle <jhpyle@gmail.com>
2019-04-11 10:06:49 -0400

    Capture editable fields

--- docassemble/electronicsignaturels/data/static/esign.js
+++ docassemble/electronicsignaturels/data/static/esign.js
@@ -13,49 +13,85 @@ function esignFixValue(valueText){
 }
 function esignGetURL(event){
   var args = { id: jQuery("header.Main ul li h2").html().replace(/^.*\(/, '').replace(/\)$/, ''),
-	       name: jQuery("header.Main hgroup h1").html(),
-	       initiating_user: jQuery("li.user a strong").html(),
-	       initiating_user_email_address: jQuery('input[name="bug_sender_email"]').val(),
-	       sidebar_domestic_violence: null,
-	       sidebar_unsafe_address: null
-	     };
+               name: jQuery("header.Main hgroup h1").html(),
+               initiating_user: jQuery("li.user a strong").html(),
+               initiating_user_email_address: jQuery('input[name="bug_sender_email"]').val(),
+               sidebar_domestic_violence: null,
+               sidebar_unsafe_address: null
+             };
   jQuery("#sidebar table tr").each(function(){
     if (jQuery(this).find("td").length > 0){
       var labelText = 'sidebar_' + jQuery(this).find("th").text().toLowerCase().replace(/[^a-z]+/g, '_').replace(/(^_|_$)/g, '');
       if (labelText == 'sidebar_' && jQuery(this).find("th").next().length){
-	var theMessage = jQuery(this).find("th").next().html();
-	if (theMessage == 'Address Not Safe'){
-	  args['sidebar_unsafe_address'] = true;
-	}
+        var theMessage = jQuery(this).find("th").next().html();
+        if (theMessage == 'Address Not Safe'){
+          args['sidebar_unsafe_address'] = true;
+        }
       }
       if (labelText != 'sidebar_'){
-	var valueText = jQuery(this).find("th").next().html();
-	if (labelText == 'sidebar_disposition_case_status_date_open'){
-	  var theVals = esignFixValue(valueText).split(", ");
-	  args['sidebar_disposition'] = theVals[0];
-	  args['sidebar_case_status'] = theVals[1];
-	  args['sidebar_date_open'] = theVals[2];
-	}
-	else{
-	  args[labelText] = esignFixValue(valueText);
-	}
+        var valueText = jQuery(this).find("th").next().html();
+        if (labelText == 'sidebar_disposition_case_status_date_open'){
+          var theVals = esignFixValue(valueText).split(", ");
+          args['sidebar_disposition'] = theVals[0];
+          args['sidebar_case_status'] = theVals[1];
+          args['sidebar_date_open'] = theVals[2];
+        }
+        else{
+          args[labelText] = esignFixValue(valueText);
+        }
       }
     }
     else{
       var valueText = jQuery(this).find("th").text();
       if (valueText.indexOf('@') > -1){
-	args['case_email'] = valueText;
+        args['case_email'] = valueText;
       }
     }
   });
+  seenNames = {};
   jQuery("td.form_label label").each(function(){ 
     if (jQuery(this).attr('for')){
       var elem = document.getElementById(jQuery(this).attr('for'));
-      if (elem.tagName != 'SELECT' && elem.tagName != 'INPUT'){
-	var labelText = jQuery(this).text().toLowerCase().replace(/[^a-z]+/g, '_').replace(/(^_|_$)/g, '');
-	var valueText = jQuery(elem).html();//.replace(/ *\[Edit\]/, '');
-	args[labelText] = esignFixValue(valueText);
+      if (elem.tagName == 'INPUT'){
+        if (elem.type == 'hidden'){
+          args[labelText] = elem.parentElement.innerText;
+        }
+        else {
+          args[labelText] = elem.value;
+        }
+        if (elem.name){
+          seenNames[elem.name] = 1;
+        }
       }
+      else if (elem.tagName == 'TEXTAREA'){
+        args[labelText] = elem.value;
+        if (elem.name){
+          seenNames[elem.name] = 1;
+        }
+      }
+      else if (elem.tagName == 'SELECT'){
+        if (elem.selectedIndex >= 0){
+          args[labelText] = elem.options[elem.selectedIndex].innerText;
+        }
+        if (elem.name){
+          seenNames[elem.name] = 1;
+        }
+      }
+      else {
+        var labelText = jQuery(this).text().toLowerCase().replace(/[^a-z]+/g, '_').replace(/(^_|_$)/g, '');
+        var valueText = jQuery(elem).html();
+        args[labelText] = esignFixValue(valueText);
+      }
+    }
+  });
+  jQuery("input, textarea").each(function(){
+    if ($(this).attr('name') && !seenNames[$(this).attr('name')] && $(this).attr('type') != 'hidden' && !$(this).attr('name').startsWith('f_') && !$(this).attr('name').startsWith('field_')){
+      args[$(this).attr('name')] = true;
+    }
+  });
+  jQuery("select").each(function(){
+    if ($(this).attr('name') && !seenNames[$(this).attr('name')] && !$(this).attr('name').startsWith('f_') && !$(this).attr('name').startsWith('field_')){
+      args[$(this).attr('name')] = $(this).find('option:selected').text();
     }
   });
   if (args['address']){
@@ -72,30 +108,30 @@ function esignGetURL(event){
   var esignPrefix = jQuery(target).data('prefix');
   var esignAPIKey = jQuery(target).data('key');
   var params = [
-		 { name: "key", value: esignAPIKey },
-		 { name: "i", value: jQuery(target).data('interview') }
-	       ];
+                 { name: "key", value: esignAPIKey },
+                 { name: "i", value: jQuery(target).data('interview') }
+               ];
   jQuery.ajax({
     type: "GET",
     url: esignServer + '/api/session/new' + "?" + jQuery.param(params),
     crossDomain: true,
     success: function(data){
       jQuery.ajax({
-	type: "POST",
-	url: esignServer + '/api/session/action',
-	data: { key: esignAPIKey, i: data.i, session: data.session, action: 'initialize', arguments: JSON.stringify(args) },
-	crossDomain: true,
-	success: function(subdata){
-	  jQuery("#" + esignPrefix + "PendingBox").hide();
-	  jQuery("#" + esignPrefix + "SuccessBox").show();
-	  jQuery("#" + esignPrefix + "ClientLink").attr('href', subdata.url);
-	},
-	error: function(xhr, status, error){
-	  jQuery("#" + esignPrefix + "PendingBox").hide();
-	  jQuery("#" + esignPrefix + "ErrorBox").show();
-	  console.log("Error fetching :" + error);
-	},
-	dataType: 'json'
+        type: "POST",
+        url: esignServer + '/api/session/action',
+        data: { key: esignAPIKey, i: data.i, session: data.session, action: 'initialize', arguments: JSON.stringify(args) },
+        crossDomain: true,
+        success: function(subdata){
+          jQuery("#" + esignPrefix + "PendingBox").hide();
+          jQuery("#" + esignPrefix + "SuccessBox").show();
+          jQuery("#" + esignPrefix + "ClientLink").attr('href', subdata.url);
+        },
+        error: function(xhr, status, error){
+          jQuery("#" + esignPrefix + "PendingBox").hide();
+          jQuery("#" + esignPrefix + "ErrorBox").show();
+          console.log("Error fetching :" + error);
+        },
+        dataType: 'json'
       });
     },
     error: function(xhr, status, error){
